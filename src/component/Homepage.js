bad59f8b7ba3e6094227f51da9c13fbfd1fd6af3
Jana Trnková <58814759+polarrrka@users.noreply.github.com>
2020-11-09 21:56:22 +0100

    new project with react

---
+++ src/component/Homepage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hats</h1>
+            <span className="subtitle">shop now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Homepage
+
