04b974d60c3a1d7a217d5ecc028568f742375aef
Jana Trnková <58814759+polarrrka@users.noreply.github.com>
2020-11-09 22:06:09 +0100

    homepage

--- src/component/Homepage.js
+++ src/component/Homepage.js
@@ -6,7 +6,7 @@ function Homepage() {
       <div className="directory-menu">
         <div className="menu-item">
           <div className="content">
-            <h1 className="title">hats</h1>
+            <h1 className="title">hoops</h1>
             <span className="subtitle">shop now</span>
           </div>
         </div>
