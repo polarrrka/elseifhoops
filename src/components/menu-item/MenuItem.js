7691df1cdcd1da78bcbc6e82b4cf375281851229
Jana Trnková <58814759+polarrrka@users.noreply.github.com>
2020-11-10 22:13:35 +0100

    ..

---
+++ src/components/menu-item/MenuItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './MenuItem.scss'
+
+const MenuItem = ({ title, imageUrl, size }) => {
+  return (
+    <div>
+        <div 
+           style={{
+              backgroundImage: `url(${imageUrl})`
+               }}
+          className={`${size} menu-item`}>
+          <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">shop now</span>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default MenuItem
---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Directory from '../../components/directory/Directory'
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+          <Directory></Directory>
+      </div>
+    </div>
+  )
+}
+
+
+export default Homepage
+
